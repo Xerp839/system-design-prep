19a5ad760284f0a95a99bd616be9d4642e5a0f6b
Xerp839 <apanishpatil839@gmail.com>
2026-08-10 21:31:58 +0530

    self iinitilization added

--- OOPS-RECAP/revision/classes_object.py
+++ OOPS-RECAP/revision/classes_object.py
@@ -1,24 +1,22 @@
 class Student:
-    #Attribute
-    name = ""
-    age = 0
-    gender = ""
-
-
     #Method
 
-    def set_info(self, name, age, gender):
+    def __init__(self, name:str , age:int , gender:str ) -> None: 
+        #Attributes
+
         self.name = name
         self.age = age
         self.gender = gender
 
-    def display(self):
+    def display(self) -> None:
         print(f"Name:{self.name}, Age:{self.age}, Gender:{self.gender}")
     
+    def get_age(self) -> int:
+        return self.age
 
 
-s1 = Student()
-s1.set_info("Valeria", 32, "Female")
-s1.display()
+s1 = Student("Valeria", 32, "Female")
+age = s1.get_age()
+print(age)
 
     
