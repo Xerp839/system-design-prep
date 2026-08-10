fad1a75227146731dbbd01901b6f418109178083
Xerp839 <apanishpatil839@gmail.com>
2026-08-10 20:41:58 +0530

    first commit

---
+++ OOPS-RECAP/revision/classes_object.py
@@ -0,0 +1,14 @@
+class Student:
+    name = ""
+    age = 0
+    gender = ""
+
+s1 = Student()
+s1.name = "Valeria"
+s1.age = 32
+s1.gender = "Female"
+print(s1)
+print(s1.name)
+print(s1.age)
+print(s1.gender)
+    
