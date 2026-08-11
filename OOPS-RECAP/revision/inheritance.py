7e778387da2cf4ea4015bc729810545f94d702d1
Xerp839 <apanishpatil839@gmail.com>
2026-08-11 13:58:16 +0530

    inheritance done

---
+++ OOPS-RECAP/revision/inheritance.py
@@ -0,0 +1,26 @@
+class Animal:
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+
+    def eat(self) -> None:
+        print("i am eating")
+    
+    def sleep(self) -> None :
+        print("I am sleeping")
+
+    
+class Dog(Animal):
+    def __init__(self, name: str, age: int, breed: str) -> None:
+        super().__init__(name, age)
+        self.breed = breed
+    
+    def bark(self) -> None:
+        print("Woof Woof")
+
+    def display(self) -> None:
+        print(f"My name is {self.name}, age is {self.age}")
+
+
+dog = Dog("Doggie", 14, "german shepard")
+dog.display()
